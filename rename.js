f4dbb0fb838a56bbd9459f3d49e5d4b9dd26f439
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2022-01-24 00:04:34 +0900

    Update rename.js

--- rename.js
+++ rename.js
@@ -8,18 +8,18 @@ module.exports = {
     async run (client, message, args){
         const member = message.mentions.members.first();
 
-        if(!member) return message.reply("Please specify a member!");
+        if(!member) return message.reply("닉네임을 변경 할 유저를 정해주세요!");
 
         const arguments = args.slice(1).join(" ");
 
-        if(!arguments) return message.reply("please specify a nickname!");
+        if(!arguments) return message.reply("변경 할 닉네임을 정해주세요!");
         
         try { 
             member.setNickname(arguments);
         } catch (err) {
             console.log(err);
             message.reply(
-                "I do not have permission to set" + member.toString() + " nickname!"
+                "권한이 없습니다" + member.toString() + " nickname!"
             );
         }
     },
