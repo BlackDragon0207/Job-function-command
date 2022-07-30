c1775f07fa802409bae16f5fdc90eccb1c294896
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2022-07-30 13:00:16 +0900

    Create image.js

---
+++ image.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const { realpathSync } = require('fs');
+const img = require('images-scraper')
+
+const google = new img({
+    puppeteer : {
+        headless : true,
+    }
+})
+
+module.exports = {
+    name: 'image',
+    aliases: ['이미지'],
+    run: async (client, message, args) => {
+        const query = args.join(" ")
+        if(!query) return 
+        let wait = await message.channel.send(`${client.emojis.cache.get('715047358873731122')} 이미지 검색중..`)
+    
+        const results = await google.scrape(query, 1)
+        const embed = new Discord.MessageEmbed()
+        .setTitle("검색 결과 입니다")
+        .setColor("GREEN")
+        .setImage(results[0].url)
+        await wait.edit('',embed)
+    }
+}
